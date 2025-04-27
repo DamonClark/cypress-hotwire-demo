d278fba2b38dc21d82bbe16167a36a94939bce33
Damon Clark <dclark312@gmail.com>
2025-04-27 08:14:18 -0400

    update

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["output"]
 
+  connect() {
+    this.outputTarget.hidden = true
+  }
+
   toggle() {
     this.outputTarget.hidden = !this.outputTarget.hidden
   }
